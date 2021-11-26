447c496e2179e93b5df43ab6799ad1aef433eb27
Haymun <Haymunjudge@gmail.com>
2021-11-25 21:19:28 -0500

    init

--- src/main/java/com/setupsafari/welcomemessage/Main.java
+++ src/main/java/com/setupsafari/welcomemessage/Main.java
@@ -2,33 +2,17 @@ package com.setupsafari.welcomemessage;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitTask;
 
-import com.setupsafari.welcomemessage.commands.RootCommand;
 import com.setupsafari.welcomemessage.listeners.PlayerJoinListener;
-import com.setupsafari.welcomemessage.tasks.Task;
 
 public class Main extends JavaPlugin {
-    
-    @Override
-    public void onEnable () {
-        this.saveDefaultConfig();
-	    
-        this.getCommand("rootcommand").setExecutor(new RootCommand());
-        
-        Bukkit.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
 
-        final long taskRepeatEvery = this.getConfig().getInt("task-repeat-every") * 20L;
-        BukkitTask task = new Task().runTaskLater(this, taskRepeatEvery);
-	    
-	// This is a regular Runnable example just in case I have a use for it,
-	// though I'm not making a Runnable example file
-	Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-			public void run() {
-				//remove the block placed.
-				System.out.println("start message XD");
-			}
-		}, 20L * 2L * 3l);
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+
+        Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(), this);
+
     }
 
     private static Main instance;
--- src/main/java/com/setupsafari/welcomemessage/listeners/PlayerJoinListener.java
+++ src/main/java/com/setupsafari/welcomemessage/listeners/PlayerJoinListener.java
@@ -1,15 +1,24 @@
 package com.setupsafari.welcomemessage.listeners;
 
 import com.setupsafari.welcomemessage.Main;
-import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class PlayerJoinListener implements Listener {
-    @EventHandler
-    public void onPlayerJoin (final PlayerJoinEvent event) {
-        final String message = Main.getInstance().getConfig().getString("messages.from-listener");
-        event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerJoin(final @NotNull PlayerJoinEvent event) {
+        @NotNull
+        final String joinMessage = Main.getInstance().getConfig().getString("messages.join-message");
+        @NotNull
+        final String motdMessage = Main.getInstance().getConfig().getString("messages.motd-message");
+
+        @NotNull
+        final Player player = event.getPlayer();
+        Bukkit.broadcastMessage(joinMessage.replaceAll("%player%", player.getDisplayName()));
+        player.sendMessage(motdMessage.replaceAll("%player", player.getDisplayName()));
     }
 }
