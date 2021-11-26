dc10a8a2b4b74851565573c02ff93cc40cb70778
Haymun <Haymunjudge@gmail.com>
2021-11-25 21:40:57 -0500

    init

--- src/main/java/com/setupsafari/welcomemessage/listeners/PlayerJoinListener.java
+++ src/main/java/com/setupsafari/welcomemessage/listeners/PlayerJoinListener.java
@@ -2,6 +2,7 @@ package com.setupsafari.welcomemessage.listeners;
 
 import com.setupsafari.welcomemessage.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,14 +12,13 @@ import org.jetbrains.annotations.NotNull;
 public class PlayerJoinListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onPlayerJoin(final @NotNull PlayerJoinEvent event) {
-        @NotNull
-        final String joinMessage = Main.getInstance().getConfig().getString("messages.join-message");
-        @NotNull
-        final String motdMessage = Main.getInstance().getConfig().getString("messages.motd-message");
+        @NotNull final String joinMessage = Main.getInstance().getConfig().getString("messages.join-message");
+        @NotNull final String motdMessage = Main.getInstance().getConfig().getString("messages.motd-message");
 
-        @NotNull
-        final Player player = event.getPlayer();
-        Bukkit.broadcastMessage(joinMessage.replaceAll("%player%", player.getDisplayName()));
-        player.sendMessage(motdMessage.replaceAll("%player", player.getDisplayName()));
+        @NotNull final Player player = event.getPlayer();
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',
+                joinMessage.replaceAll("%player%", player.getDisplayName())));
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                motdMessage.replaceAll("%player", player.getDisplayName())));
     }
 }
